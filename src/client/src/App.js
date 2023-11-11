65c88d388361c62286616734d9546fc508c74869
Lauri Sarap <lauri.aardla@gmail.com>
2023-11-11 15:12:50 +0200

    Started work on UI

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {set} from "express/lib/application";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  const sendMessage = async () => {
+    if (!input) return;
+
+    // Add user message to messages array
+    const updatedMessages = [...messages, { text: input, sender: 'user' }];
+
+    // TODO: Send 'input' to your backend and get the response from OpenAI
+    // TODO: Add AI response to message array
+
+    // AI response example for testing purposes
+    const aiResponse = `Simulated response for: ${input}`;
+    updatedMessages.push({ text: aiResponse, sender: 'ai' });
+
+    // Update the state with new messages array
+    setMessages(updatedMessages)
+
+    // Clear input after sending
+    setInput('');
+  };
+
+  return(
+      <div className="App">
+       <div className="chatbox">
+         <div className="messages">
+           {messages.map((message, index) => (
+               <div key={index} className={`message ${message.sender}`}>
+                 {message.text}
+               </div>
+           ))}
+         </div>
+         <div className="input-area">
+           <input
+               type="text"
+               value={input}
+               onChange={(e) => setInput(e.target.value)}
+               onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+           />
+           <button onClick={sendMessage}>Send</button>
+         </div>
+       </div>
+      </div>
   );
 }
 
